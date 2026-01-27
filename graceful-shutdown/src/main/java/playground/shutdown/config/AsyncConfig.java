38ea97bde742e539ec6e29716e05326fd270ce0d
jonghyeok-97 <dkswhdgur1209@naver.com>
2026-01-27 13:03:15 +0900

    [Graceful Shutdown] 애플리케이션과 비동기 쓰레드 풀 간의 우아한 종료 테스트를 위한 코드 추가

--- graceful-shutdown/src/main/java/playground/shutdown/config/AsyncConfig.java
+++ graceful-shutdown/src/main/java/playground/shutdown/config/AsyncConfig.java
@@ -1,28 +1,110 @@
 package playground.shutdown.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
 @Slf4j
 @Configuration
 public class AsyncConfig {
 
     @Bean
-    public TaskExecutor taskExecutor() {
+    public ThreadPoolTaskExecutor taskExecutor1() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
         taskExecutor.setCorePoolSize(1);
-        taskExecutor.setMaxPoolSize(3);
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setAwaitTerminationSeconds(30);
 
-        // Core Thread 유휴 시간 설정
-        taskExecutor.setAllowCoreThreadTimeOut(true); // 기본 값: false
-        taskExecutor.setKeepAliveSeconds(5); // 기본 값: 60초
-        taskExecutor.setQueueCapacity(0);
+        taskExecutor.setThreadNamePrefix("Async1-");
 
         taskExecutor.initialize();
 
         return taskExecutor;
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor2() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(1);
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setAwaitTerminationSeconds(15);
+
+        taskExecutor.setThreadNamePrefix("Async2-");
+
+        taskExecutor.initialize();
+
+        return taskExecutor;
+    }
+
+    @Component
+    public static class MyPhase implements SmartLifecycle {
+        private volatile boolean running = false;
+
+        @Override
+        public void start() {
+            log.info("HighPhaseComponent 시작");
+            running = true;
+        }
+
+        @Override
+        public void stop() {
+            log.info("[Phase 1000 - Bean1] stop() 시작");
+            try {
+                Thread.sleep(15000); // 15초
+            } catch (InterruptedException e) {}
+            log.info("[Phase 1000 - Bean1] stop() 종료");
+        }
+
+        @Override
+        public int getPhase() {
+            return 1000;
+        }
+        @Override
+        public boolean isRunning() {
+            return running;
+        }
+    }
+
+    @Component
+    static public class LowPhaseComponent implements SmartLifecycle {
+
+        private volatile boolean running = false;
+
+        @Override
+        public void start() {
+            log.info("LowPhaseComponent 시작");
+            running = true;
+        }
+
+        @Override
+        public boolean isRunning() {
+            return running;
+        }
+
+        @Override
+        public void stop() {
+            log.info("[Phase 1000 - Bean2] stop() 시작");
+            try {
+                Thread.sleep(3000); // 3초
+            } catch (InterruptedException e) {}
+            log.info("[Phase 1000 - Bean2] stop() 종료");
+        }
+
+        @Override
+        public void stop(Runnable callback) {
+            log.info("[Phase 1000 - Bean2] stop() 시작");
+            try {
+                Thread.sleep(3000); // 3초
+            } catch (InterruptedException e) {}
+            log.info("[Phase 1000 - Bean2] stop() 종료");
+        }
+
+        @Override
+        public int getPhase() {
+            return 1000;
+        }
     }
 }
--- graceful-shutdown/src/main/java/playground/shutdown/config/TestController.java
+++ graceful-shutdown/src/main/java/playground/shutdown/config/TestController.java
@@ -1,40 +1,55 @@
 package playground.shutdown.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.task.TaskExecutor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TestController {
 
-    private final TaskExecutor taskExecutor;
+    private final ThreadPoolTaskExecutor taskExecutor1;
+    private final ThreadPoolTaskExecutor taskExecutor2;
 
-    @GetMapping("/kill-core-threads")
+
+    @GetMapping("/tasks")
     public String test() throws InterruptedException {
-        ThreadPoolTaskExecutor executor = (ThreadPoolTaskExecutor) taskExecutor;
-        System.out.println("executor.getKeepAliveSeconds() = " + executor.getKeepAliveSeconds());
-        System.out.println("executor.getCorePoolSize() = " + executor.getCorePoolSize());
-        System.out.println("executor.getMaxPoolSize() = " + executor.getMaxPoolSize());
-
-        // Max Thread 까지 실행
-        for (int i = 0; i < 3; i++) {
-            executor.submit(() -> {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+//        ThreadPoolTaskExecutor taskExecutor = (ThreadPoolTaskExecutor) taskExecutor1;
+        log.info("비동기 작업 제출 (80초 소요 예상)...");
+        taskExecutor1.submit(() -> {
+            try {
+                for (int i = 1; i <= 80; i++) {
+                    Thread.sleep(1000); // 1초씩 30번 대기
+                    log.info("비동기 작업 진행 중: {}초 경과", i);
                 }
-            });
-        }
-        System.out.println("살아있는 쓰레드 수 = " + executor.getPoolSize());
+                log.info("비동기 작업 완료!");
+            } catch (InterruptedException e) {
+                log.warn("비동기 작업이 강제 종료(Interrupted) 되었습니다!");
+            }
+        });
 
-        // 10초간 Blocking
-        Thread.sleep(10000);
-        System.out.println("10초 뒤 살아있는 쓰레드 수 = " + executor.getPoolSize());
+        return "작업이 제출되었습니다. 이제 서버를 종료(Ctrl+C)하여 로그를 확인하세요.";
+    }
 
-        return "test";
+    @GetMapping("/tasks-2")
+    public String test2() throws InterruptedException {
+//        ThreadPoolTaskExecutor taskExecutor = (ThreadPoolTaskExecutor) taskExecutor2;
+        log.info("비동기 작업 제출 (30초 소요 예상)...");
+        taskExecutor2.submit(() -> {
+            try {
+                for (int i = 1; i <= 30; i++) {
+                    Thread.sleep(1000); // 1초씩 30번 대기
+                    log.info("비동기 작업 진행 중: {}초 경과", i);
+                }
+                log.info("비동기 작업 완료!");
+            } catch (InterruptedException e) {
+                log.warn("비동기 작업이 강제 종료(Interrupted) 되었습니다!");
+            }
+        });
+
+        return "작업이 제출되었습니다. 이제 서버를 종료(Ctrl+C)하여 로그를 확인하세요.";
     }
 }
